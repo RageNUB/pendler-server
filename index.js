d478e201cc6c62ba812dc8028d74a9c4e35d5360
RageNUB <jirimon39@gmail.com>
2023-07-13 04:44:45 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// const express = require('express');
+// const cors = require('cors');
+// require('dotenv').config()
+// const app = express();
+// const port = process.env.PORT || 5000;
+
+// const corsConfig = {
+//     origin: "*",
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE"]
+// }
+// app.use(cors(corsConfig))
+// app.options("", cors(corsConfig))
+// app.use(express.json());
+
+// app.get("/", (req, res) => {
+//     res.send("Pendler server is running");
+// })
+
+// app.listen(port, () => {
+//     console.log(`Pendler server is running on port: ${port}`);
+// })
